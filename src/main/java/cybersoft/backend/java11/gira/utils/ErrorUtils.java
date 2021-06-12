be2746ceb4160d056bf296f13b5443b30b1e22cd
thienanh <todiepthienanh@gmail.com>
2021-06-12 19:42:15 +0700

    update buoi4

---
+++ src/main/java/cybersoft/backend/java11/gira/utils/ErrorUtils.java
@@ -0,0 +1,25 @@
+package cybersoft.backend.java11.gira.utils;
+
+import java.util.LinkedList;
+import java.util.List;
+
+import org.springframework.validation.ObjectError;
+
+public class ErrorUtils {
+	public static List<String> getErrorMessages(List<ObjectError> errors){
+		List<String> messages = new LinkedList<>();
+		
+		for (ObjectError error : errors) {
+			messages.add(error.getDefaultMessage());
+		}
+		
+		return messages;
+		
+	}
+	
+	public static List<String> errorOf(String message){
+		List<String> messages = new LinkedList<String>();
+		messages.add(message);
+		return messages;
+	}
+}
---
+++ src/main/java/cybersoft/backend/java11/gira/utils/ListUtils.java
@@ -0,0 +1,8 @@
+package cybersoft.backend.java11.gira.utils;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class ListUtils {
+	public static List<String> emptyStringList = new LinkedList<>();
+}
